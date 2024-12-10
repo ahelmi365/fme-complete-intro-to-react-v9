7cc91d7f60c798910384a93bb466ddaf905d5dc7
Ali Helmi <ahelmi365@gmail.com>
2024-12-10 17:07:05 +0200

    import react and createRoot

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 const Piza = (props) => {
   return React.createElement(
     "div",
@@ -32,5 +35,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
