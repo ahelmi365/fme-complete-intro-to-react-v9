4241daa32bb7405c566a2adeca98ce5ce222b021
Ali Helmi <ahelmi365@gmail.com>
2024-12-12 16:15:22 +0200

    use getPastOrders and handle error

--- src/PastOrders.jsx
+++ src/PastOrders.jsx
@@ -1,12 +1,61 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+import getPastOrders from "./api/getPastOrders";
+
 const PastOrders = () => {
+  const [page, setPage] = useState(1);
+
+  const { isLoading, error, data, isError } = useQuery({
+    queryKey: ["past-orders", page],
+    queryFn: () => getPastOrders(page),
+    staleTime: 30000,
+  });
+  console.log({ isError, error });
+  if (isLoading) {
+    return (
+      <div className="past-orders">
+        <h2>LOADING...</h2>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="past-orders">
+        <h2>Error... {error.message}</h2>
+      </div>
+    );
+  }
   return (
-    <div className="">
-      <h2>PastOrders</h2>
-      <ul>
-        <li>order-1</li>
-        <li>order-2</li>
-        <li>order-3</li>
-      </ul>
+    <div className="past-orders">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Date</td>
+            <td>Time</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((order) => (
+            <tr key={order.order_id}>
+              <td>{order.order_id}</td>
+              <td>{order.date}</td>
+              <td>{order.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="pages">
+        <button disabled={page <= 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        <div>{page}</div>
+        <button disabled={data.length < 10} onClick={() => setPage(page + 1)}>
+          Next
+        </button>
+      </div>
     </div>
   );
 };
