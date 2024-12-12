6f92a2eea2f7e15cce58ed17e1ca9f4d3430db7a
Ali Helmi <ahelmi365@gmail.com>
2024-12-12 14:03:10 +0200

    init pastorders

---
+++ src/PastOrders.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PastOrders = () => {
+  return (
+    <div className="">
+      <h2>PastOrders</h2>
+      <ul>
+        <li>order-1</li>
+        <li>order-2</li>
+        <li>order-3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default PastOrders;
