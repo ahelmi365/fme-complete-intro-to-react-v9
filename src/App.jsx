b290a9622c66095183daf09892b21990112b8e1d
Ali Helmi <ahelmi365@gmail.com>
2024-12-12 10:23:05 +0200

    init tanstack/router with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,14 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
-import { useEffect, useState } from "react";
 import { StrictMode } from "react";
 
-import PizzaOfTheDay from "./PizzaOfTheDay";
-import { CartContext } from "./contexts";
-import Header from "./Header";
-import Pizza from "./Pizza";
-import Order from "./Order";
+import { routeTree } from "./routeTree.gen";
 
+const router = createRouter(routeTree);
 const App = () => {
-  const [pizzas, setPizzas] = useState([]);
-  const getPizzas = async () => {
-    const res = await fetch("/api/pizzas");
-    const data = await res.json();
-    setPizzas(data);
-  };
-
-  useEffect(() => {
-    getPizzas();
-  }, []);
-  const renderedPizaas = pizzas.map((piza) => (
-    <Pizza
-      id={piza.id}
-      name={piza.name}
-      description={piza.description}
-      image={piza.image}
-      key={piza.id}
-    />
-  ));
-
-  const cartHook = useState([]);
   return (
     <StrictMode>
-      <CartContext.Provider value={cartHook}>
-        <div className="app-container">
-          <Header />
-          <Order />
-          <PizzaOfTheDay />
-        </div>
-      </CartContext.Provider>
+      <RouterProvider router={router} />
     </StrictMode>
   );
 };
---
+++ src/routes/__root.jsx
@@ -0,0 +1,25 @@
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { useState } from "react";
+
+import PizzaOfTheDay from "../PizzaOfTheDay";
+import { CartContext } from "../contexts";
+import Header from "../Header";
+
+export const Route = createRootRoute({
+  component: () => {
+    const cartHook = useState([]);
+    return (
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div className="app-container">
+            <Header />
+            <Outlet />
+            <PizzaOfTheDay />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+      </>
+    );
+  },
+});
