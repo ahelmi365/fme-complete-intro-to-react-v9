f45a9f222f786ba219b7ee47a255f160f937fd3c
Ali Helmi <ahelmi365@gmail.com>
2024-12-12 14:34:01 +0200

    configure QueryClientProvider in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
@@ -5,10 +6,13 @@ import { StrictMode } from "react";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
 const App = () => {
   return (
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 };
