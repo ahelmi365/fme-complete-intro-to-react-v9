6e1cddc790bd50c04d09c69f176a08a6df998e3c
Ali Helmi <ahelmi365@gmail.com>
2024-12-15 15:26:12 +0200

    show modal with past orders

--- src/PastOrders.jsx
+++ src/PastOrders.jsx
@@ -1,19 +1,34 @@
 import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
+import useCurrencyINTL from "./useCurrencyINTL";
 import getPastOrders from "./api/getPastOrders";
+import getPastOrder from "./api/getPastOrder";
 import Modal from "./Modal";
 
 const PastOrders = () => {
   const [page, setPage] = useState(1);
-  const [showModal, setShowModal] = useState(false);
+  const [focusedOrder, setFocusedOrder] = useState(null);
+  const currencyINTL = useCurrencyINTL();
 
   const { isLoading, error, data, isError } = useQuery({
     queryKey: ["past-orders", page],
     queryFn: () => getPastOrders(page),
     staleTime: 30000,
   });
-  console.log({ isError, error });
+
+  const {
+    isLoading: isLoadingOrder,
+    error: errorOrder,
+    data: pastOrderData,
+    isError: isErrorOrder,
+  } = useQuery({
+    queryKey: ["past-order", focusedOrder],
+    queryFn: () => getPastOrder(focusedOrder),
+    staleTime: 24 * 60 * 60 * 1000,
+    enabled: !!focusedOrder,
+  });
+
   if (isLoading) {
     return (
       <div className="past-orders">
@@ -42,7 +57,10 @@ const PastOrders = () => {
           {data?.map((order) => (
             <tr key={order.order_id}>
               <td>
-                <button type="button" onClick={() => setShowModal(true)}>
+                <button
+                  type="button"
+                  onClick={() => setFocusedOrder(order.order_id)}
+                >
                   {order.order_id}
                 </button>
               </td>
@@ -63,12 +81,45 @@ const PastOrders = () => {
         </button>
       </div>
 
-      {showModal && (
+      {focusedOrder && (
         <Modal>
           <>
-            hello from modal
-            <button type="button" onClick={() => setShowModal(false)}>
-              Close Modal
+            <h2>Order #{focusedOrder}</h2>
+
+            {isLoadingOrder && <p>Loading</p>}
+            {pastOrderData && (
+              <table>
+                <thead>
+                  <tr>
+                    <td>Image</td>
+                    <td>Name</td>
+                    <td>Size</td>
+                    <td>Quantity</td>
+                    <td>Price</td>
+                    <td>Total</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {pastOrderData.orderItems.map((pizza) => (
+                    <tr key={`${pizza.pizzaTypeId}_${pizza.size}`}>
+                      <td>
+                        <img src={pizza.image} alt={pizza.name} />
+                      </td>
+                      <td>{pizza.name}</td>
+                      <td>{pizza.size}</td>
+                      <td>{pizza.quantity}</td>
+                      <td>{currencyINTL.format(pizza.price)}</td>
+                      <td>{currencyINTL.format(pizza.total)}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+
+            {isErrorOrder && <p>{errorOrder.message}</p>}
+
+            <button type="button" onClick={() => setFocusedOrder(null)}>
+              Close
             </button>
           </>
         </Modal>
