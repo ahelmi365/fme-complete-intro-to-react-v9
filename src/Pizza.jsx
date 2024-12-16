96e760bd12949bc0439b031bfd001bccd959919d
Ali Helmi <ahelmi365@gmail.com>
2024-12-16 12:58:27 +0200

    add image backup

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -3,7 +3,7 @@ const Pizza = (props) => {
     <div className="pizza" id={props.id}>
       <h2>{props.name}</h2>
       <p>{props.description}</p>
-      <img src={props.image} alt={props.name} />
+      <img src={props?.image || "https://picsum.photos/200"} alt={props.name} />
     </div>
   );
 };
